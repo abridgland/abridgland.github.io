22d7c45d5b9ad75b13a893eed5869967c6ce0c9f
Alex Bridgland <alex.bridgland@gmail.com>
2017-12-08 02:46:33 +0000

    Update blog theme to more-or-less final version.

--- _includes/plots/gaussian-processes-1/gen_plots.py
+++ _includes/plots/gaussian-processes-1/gen_plots.py
@@ -15,7 +15,7 @@ def save_plot(plot, title):
         html_file.write(plot_html)
 
 def plot_unit_gaussian_samples(D):
-    p = figure(plot_width=800, plot_height=500, title='Samples from a unit {}D Gaussian'.format(D))
+    p = figure(plot_width=800, plot_height=500, title='Samples from a unit {}D Gaussian'.format(D), sizing_mode='scale_width')
 
     xs = np.linspace(0, 1, D)
     for color in Category10[10]:
@@ -46,7 +46,7 @@ d = k(x, y)
 color_mapper = LinearColorMapper(palette="Plasma256", low=0, high=1)
 
 p = figure(plot_width=400, plot_height=400, x_range=(-2, 2), y_range=(-2, 2),
-           title='Visualisation of k(x, x\')', x_axis_label='x', y_axis_label='x\'', toolbar_location=None)
+           title='Visualisation of k(x, x\')', x_axis_label='x', y_axis_label='x\'', toolbar_location=None, sizing_mode='scale_width')
 p.image(image=[d], color_mapper=color_mapper, x=-2, y=-2, dw=4, dh=4)
 
 color_bar = ColorBar(color_mapper=color_mapper, ticker=BasicTicker(),
@@ -56,7 +56,7 @@ p.add_layout(color_bar, 'right')
 
 save_plot(p, 'kernel_viz')
 
-p = figure(plot_width=800, plot_height=500)
+p = figure(plot_width=800, plot_height=500, title='Samples from a 20D Gaussian with kernel smoothing', sizing_mode='scale_width')
 D = 20
 xs = np.linspace(0, 1, D)
 for color in Category10[10]:
@@ -71,7 +71,7 @@ xs = np.linspace(-5, 5, n)
 K = k(xs, xs, sigma=1, l=1)
 mu = m(xs)
 
-p = figure(plot_width=800, plot_height=500)
+p = figure(plot_width=800, plot_height=500, title='5 samples from GP prior', sizing_mode='scale_width')
 
 for color in Category10[5]:
     ys = np.random.multivariate_normal(mu, K)
@@ -92,7 +92,7 @@ xs = np.linspace(-5.0, 3.5, 100)
 ys = f(xs)
 
 p = figure(plot_width=800, plot_height=400, x_axis_label='x', y_axis_label='f(x)',
-           title='The hidden function f(x)')
+           title='The hidden function f(x)', sizing_mode='scale_width')
 p.line(xs, ys, line_width=2)
 
 save_plot(p, 'hidden_function')
@@ -111,7 +111,7 @@ K_sTKinv = np.matmul(K_s.T, np.linalg.pinv(K))
 mu_s = m(x_s) + np.matmul(K_sTKinv, y_obs - m(x_obs))
 Sigma_s = K_ss - np.matmul(K_sTKinv, K_s)
 
-p = figure(plot_width=800, plot_height=600, y_range=(-7, 8))
+p = figure(plot_width=800, plot_height=600, y_range=(-7, 8), title='GP posterior', sizing_mode='scale_width')
 
 y_true = f(x_s)
 p.line(x_s, y_true, line_width=3, color='black', alpha=0.4, line_dash='dashed', legend='True f(x)')
--- _posts/2017-12-07-gaussian-processes-1.md
+++ _posts/2017-12-07-gaussian-processes-1.md
@@ -1,11 +1,9 @@
 ---
 layout: post
 title: Introduction to Gaussian Processes - Part I
+permalink: /posts/gaussian-processes-1
 ---
 
-Author: [@alex_bridgland](https://twitter.com/alex_bridgland) \|  [@bridgo](https://github.com/Bridgo)
-
-***
 This is a notebook that I originally wrote when learning about Gaussian Processes. I thought it might be helpful for others in the same situation so I added a writeup to go with the code - hopefully it offers a bit of a new perspective!
 
 I aimed for it to give a more visual / intuitive introduction without totally abandoning the theory. And since it's a notebook you should be able to get an even better understanding by downloading the notebook and playing with the code. Any/all feedback appreciated!
@@ -13,6 +11,10 @@ I aimed for it to give a more visual / intuitive introduction without totally ab
 ## What is a Gaussian Process and why would I use one?
 ***
 A Gaussian process is a powerful model that can be used to represent a distribution over functions. Most techniques in machine learning tend to avoid this by parameterising functions and modeling their parameters (e.g. the weights in linear regression). However by modeling the function itself we can get useful uncertainty information. Quantifying uncertainty can be extremely valuable - for example if we can explore (request more data) we can chose to explore the areas we are least certain about to be as efficient as possible. For more information on the importance of uncertainty modeling see [this article](http://mlg.eng.cam.ac.uk/yarin/blog_3d801aa532c1ce.html) by Yarin Gal.
+
+> If you give me several pictures of cats and dogs – and then you ask me to classify a new cat photo – I should return a prediction with rather high confidence. But if you give me a photo of an ostrich and force my hand to decide if it's a cat or a dog – I better return a prediction with very low confidence.
+>
+> Yarin Gal
 
 For this introduction we will consider the standard supervised learning setting (without noise):
 - We assume there is some hidden function \\( f:\mathbb{R}^D\rightarrow\mathbb{R} \\) that we want to model.
